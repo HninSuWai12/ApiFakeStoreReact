470862137073bc17ab9e6f66060745666369e778
Hninsuwai1234 <hninsuwai@ilbcedu.com>
2023-09-21 22:06:57 +0630

    Add line of code

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStateContex } from '../context/StateContex'; 
+import Empty from './Empty';
 // Correct the import path and name
 import { AiFillDelete } from 'react-icons/ai';
 const Cart = () => {
@@ -10,9 +11,10 @@ const Cart = () => {
   //console.log(cart);
   
   return (
-    <div className="flex items-center gap-5">
+    <div className='grid grid-cols-4'>
+      <div className="flex items-center gap-5">
       {cartData?.map(item => (
-        <div key={item.id} className="flex items-center gap-4" >
+        <div key={item.id} className="flex flex-row flex-wrap items-center gap-4" >
           <img src={item?.image} className='h-32 border-2 rounded p-4' alt="" />
           <div className="">
             <h3>{item?.title}</h3>
@@ -22,6 +24,17 @@ const Cart = () => {
           </div>
         </div>
       ))}
+       <div className="">
+    <Empty/>
+  </div>
+  <div className="col-span-1">
+    <div className="">
+      <h1 >Total Price_10000</h1>
+      <button className='px-5 py-2 bg-info text-primary rounded shadow-lg'>Check Out</button>
+    </div>
+  </div>
+    </div>
+   
     </div>
   );
 }
